587de94253b7e9893e379c558333571bcd5222c3
Richa176 <rich.bhasin@gmail.com>
2020-12-02 19:53:08 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,79 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+var polygon_img;
+function preload(){
+  polygon_img=loadImage("polygon.png");
+}
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  stand1 = new Stand(390,300,250,10);
+  stand2 = new Stand(700,200,200,10);
+ 
+  //level one
+  block1 = new Block(300,275,30,40);
+  console.log(block1);
+  
+  //level two
+  
+  //level three
+  
+  //top
+  
+
+  //set 2 for second stand
+  //level one
+ 
+  //level two
+  
+  //top
+  
+  //ball holder with slings
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  slingShot = new Slingshot(this.ball,{x:100,y:200});
+
+}
+function draw() {
+  background(56,44,44); 
+ 
+  //Engine.update(engine);
+  //text(mouseX + ',' + mouseY, 10, 15);
+  textSize(20);
+  fill("lightyellow");
+  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
+
+  ground.display();
+  stand1.display();
+  stand2.display();
+
+  strokeWeight(2);
+  stroke(15);
+  fill("skyblue");
+  block1.display();
+  
+  imageMode(CENTER)
+  image(polygon_img ,ball.position.x,ball.position.y,40,40);
+
+  slingShot.display();
+}
+
+function mouseDragged(){
+  //add code
+}
+
+function mouseReleased(){
+  //add code
+}
