6a994de710173c660c03cc810db6c710a005c50f
Richa176 <69035220+Richa176@users.noreply.github.com>
2021-06-30 19:32:43 +0530

    updated sketch

--- sketch.js
+++ sketch.js
@@ -21,15 +21,28 @@ function setup() {
   stand1 = new Stand(390,300,250,10);
   stand2 = new Stand(700,200,200,10);
  
-  //level one
+  
+    //level one
   block1 = new Block(300,275,30,40);
   console.log(block1);
-  
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
   //level two
-  
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
   //level three
-  
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
   //top
+  block16 = new Block(390,155,30,40);
   
 
   //set 2 for second stand
